bcf02b5ec3b096283472263db275c1ad4d2cd729
Kate Rdz <a01234357@tec.mx>
2024-08-08 12:35:52 -0600

    js, trabajo lab3

---
+++ js/introjs.js
@@ -0,0 +1,87 @@
+// consola (log, info, warn, error, assert)
+console.log("hola mundo!");
+console.info("esto es informacion importante");
+console.warn("Cuidado!!");
+console.error("Ya te equivocaste...");
+console.assert(1 == 1);
+console.assert(1 == 0);
+
+
+// variables, constantes
+let nombre= 'Kate';
+console.log("Hola " + nombre);
+console.log(`${nombre} esta programando`);
+
+const precio_sb = 92;
+
+// Alcance de las variables
+
+
+
+
+
+// alert, prompt, confirm
+
+
+
+
+// funciones tradicionales
+
+
+
+
+// funciones modernas
+
+
+
+
+// arreglos
+
+
+
+
+//const arreglo = ["Elemento"];
+
+
+
+
+//const arreglo = new Array() 
+
+
+
+
+//arreglo.push("Otro elemento");
+
+
+
+
+//arreglo[10] = "Uno más";
+
+
+
+
+//arreglos asociativos
+
+
+
+
+//recorrido tradicional del arreglo
+
+
+
+
+
+//for (let i = 0; i < arreglo.length; i++) {
+//    console.log(arreglo[i]);
+//}
+
+
+
+
+//recorridos alternativos del arreglo
+//for(let posicion in arreglo) {
+//    console.log(posicion);
+//}
+//Objetos
+//const objeto = {} 
+// modificar html
