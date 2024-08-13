fa3dd3c0cd6a02d4580dc2e80f3914aa4939a14d
Kate Rdz <a01234357@tec.mx>
2024-08-13 17:26:42 -0600

    Lab 4, inciso B

--- jsLab4/js-Lab4.js
+++ jsLab4/js-Lab4.js
@@ -5,17 +5,8 @@ const edad = prompt("Que edad tienes?");
 document.write(`Tienes ${edad} años sin bañarte LOL`);
 
 
-function sumaRandom (){
-    const start = new Date();
-    const num1 = math.random();
-    const num2 = math.random();
+const num1 = math.random();
+const num2 = math.random();
 
-    const suma = prompt(`La suma de ${num1} + ${num2} es:`);
-    console.assert(suma == num1+num2);
-    
-
-    const end = new Date();
-    document.write(end.getTime() - start.getTime(), "ms");
-}
-
-sumaRandom();
+const suma = prompt(`La suma de ${num1} + ${num2} es:`);
+console.assert(suma == num1+num2);
