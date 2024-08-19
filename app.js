753f0f29d2f82c4d3b55fd36ed89c409da15547e
Kate Rdz <a01234357@tec.mx>
2024-08-19 12:27:53 -0600

    Generar pagina de respuesta para Preparar

--- app.js
+++ app.js
@@ -28,6 +28,84 @@ const server = http.createServer((request, response) => {
     console.log(request.url);
     response.setHeader('Content-Type', 'text/html');
     if (request.url == "/preparar"){
+        response.write(`
+            <!DOCTYPE html>
+            <html data-theme="light">
+                <head>
+                    <meta charset="utf-8">
+                    <meta name="viewport" content="width=device-width, initial-scale=1">
+                    <title>Chilaquiles</title>
+                    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@1.0.2/css/bulma.min.css">
+                </head>
+                <body>
+                    <nav class="navbar" role="navigation" aria-label="main navigation">
+                        <div class="navbar-brand">
+                            <a class="navbar-item" href="https://tecfood.tec.mx/es/monterrey/chilaquiles-tec">
+                                <img src="https://tecfood.tec.mx/sites/g/files/vgjovo1061/files/styles/de2e/public/2022-09/CCM-chilaquiles-logo.png?itok=3wX3kVxi">
+                            </a>
+                            <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+                                <span aria-hidden="true"></span>
+                                <span aria-hidden="true"></span>
+                                <span aria-hidden="true"></span>
+                                <span aria-hidden="true"></span>
+                            </a>
+                        </div>
+            
+                        <div id="navbarBasicExample" class="navbar-menu">
+                            <div class="navbar-start">
+                                <a class="navbar-item">
+                                Inicio
+                                </a>
+                            </div>
+                            <div class="navbar-end">
+                                <div class="navbar-item">
+                                    <div class="buttons">
+                                        <a class="button is-primary">
+                                        <strong>Registrarse</strong>
+                                        </a>
+                                        <a class="button is-light">
+                                        Iniciar sesión
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </nav>
+                    <section class="section">
+                        <div class="container">
+                            <header>
+                                <h1 class="title">Preparar chilaquiles</h1>
+                            </header>
+                            <main>
+                                <form action="/preparar" method="POST">
+                                <div class="field">
+                                    <label class="label" for="nombre">Nombre</label>
+                                    <div class="control">
+                                        <input id="nombre" name="nombre" class="input" type="text" placeholder="Lalo">
+                                    </div>
+                                </div>
+                                    
+                                </form>
+                            </main>
+                            <footer class="footer">
+                                <div class="content has-text-centered">
+                                    <p>
+                                        <strong>Bulma</strong> by <a href="https://jgthms.com">Jeremy Thomas</a>.
+                                        The source code is licensed
+                                        <a href="https://opensource.org/license/mit">MIT</a>. The
+                                        website content is licensed
+                                        <a href="https://creativecommons.org/licenses/by-nc-sa/4.0//"
+                                        >CC BY NC SA 4.0</a
+                                        >.
+                                    </p>
+                                </div>
+                            </footer>
+                        </div>
+                    </section>
+                    <script src="js/poe.js"></script>
+                </body>
+            </html>
+            `);    
 
     } else {
     response.write(`
