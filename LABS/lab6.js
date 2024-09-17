32dd3e39a157d4f8413fe505e06df59eb9739d85
Kate Rdz <a01234357@tec.mx>
2024-09-17 11:37:40 -0600

    Lab 6

--- LABS/lab6.js
+++ LABS/lab6.js
@@ -1,6 +1,33 @@
-const buttonCherie = document.getElementById("itemCherie");
-const buttonMartini = document.getElementById("itemMartini");
-const buttonFerrari = document.getElementById("itemFerrari");
+const agregarCherie = document.getElementById("1");
+const agregarMartini = document.getElementById("2");
+const agregarFerrari = document.getElementById("3");
+
+agregarCherie.addEventListener("click", addC);
+agregarMartini.addEventListener("click", addM);
+agregarFerrari.addEventListener("click", addF);
+
+const buttonCheckout = document.getElementById("checkout");
+
+
+
+//Count Cherie
+let count = 0;
+let sum = 0;
+
+// Count Martini
+let count1 = 0;
+let sum1 = 0;
+
+//Count Ferrari
+let count2 = 0;
+let sum2 = 0;
+
+// Subtotal y Total
+let sumaSubtotal = 0;
+let sumaTotal = (sumaSubtotal * 0.16) + sumaSubtotal;
+
+let cart = {};
+
 
 const gallery = [
     {
@@ -44,20 +71,101 @@ var cont = document.getElementById("FERRARIT");
 var content = document.createTextNode(`${gallery[2].descripcion}`);
 cont.appendChild(content);
 
-buttonCherie.onclick=()=>{
-    const imagen=document.getElementById("orderPlaced");
-    imagen.alt="Orden";
-    imagen.src="https://static.knowledgebase.ai/upload/10142137/464c761d20-fabd610b2e6a53bf15e6.png";
-};
+buttonCheckout.onclick=()=>{
+    const img = document.getElementById("orderPlaced");
+    img.src = "https://support.jamestowndistributors.com/hc/article_attachments/360069169914/mceclip1.png";
+    src.appendChild(img);
+    
+}
 
-buttonMartini.onclick=()=>{
-    const imagen=document.getElementById("orderPlaced");
-    imagen.alt="Orden";
-    imagen.src="https://static.knowledgebase.ai/upload/10142137/464c761d20-fabd610b2e6a53bf15e6.png";
-};
+function addC(event) {
+    let price = Number(event.target.dataset.price);
+    let title = event.target.dataset.title;
+    let id = event.target.dataset.id;
 
-buttonFerrari.onclick=()=>{
-    const imagen=document.getElementById("orderPlaced");
-    imagen.alt="Orden";
-    imagen.src="https://static.knowledgebase.ai/upload/10142137/464c761d20-fabd610b2e6a53bf15e6.png";
-};
+if (id in cart) {
+    cart[id].qty++;
+} else {
+    let cartItem = {
+        title: title,
+        price: price,
+        qty: 1
+    };
+    cart[id] = cartItem
+}
+
+    count++;
+    sum += price;
+    sumaSubtotal += sum;
+    sumaTotal = (sumaSubtotal * 0.16) + sumaSubtotal;
+    updateCart();
+}
+
+function addM(event) {
+    let price = Number(event.target.dataset.price);
+    let title = event.target.dataset.title;
+    let id = event.target.dataset.id;
+
+if (id in cart) {
+    cart[id].qty++;
+} else {
+    let cartItem = {
+        title: title,
+        price: price,
+        qty: 1
+    };
+    cart[id] = cartItem
+}
+
+    count1++;
+    sum1 += price;
+    sumaSubtotal += sum1;
+    sumaTotal = (sumaSubtotal * 0.16) + sumaSubtotal;
+    updateCartM();
+}
+
+function addF(event) {
+    let price = Number(event.target.dataset.price);
+    let title = event.target.dataset.title;
+    let id = event.target.dataset.id;
+
+if (id in cart) {
+    cart[id].qty++;
+} else {
+    let cartItem = {
+        title: title,
+        price: price,
+        qty: 1
+    };
+    cart[id] = cartItem
+}
+
+    count2++;
+    sum2 += price;
+    sumaSubtotal += sum2;
+    sumaTotal = (sumaSubtotal * 0.16) + sumaSubtotal;
+    updateCartF();
+}
+
+function updateCart() {
+    document.getElementById("sum").textContent = sum;
+    document.getElementById("count").textContent = count;
+    subtotalyTotal();
+}
+
+function updateCartM() {
+    document.getElementById("sum1").textContent = sum1;
+    document.getElementById("count1").textContent = count1;
+    subtotalyTotal();
+}
+
+function updateCartF() {
+    document.getElementById("sum2").textContent = sum2;
+    document.getElementById("count2").textContent = count2;
+    subtotalyTotal();
+}
+
+function subtotalyTotal(){
+    document.getElementById("subtotal").textContent = sumaSubtotal;
+    document.getElementById("total").textContent = sumaTotal;
+}
